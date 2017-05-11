5307dd9d9703c473959914e151fce318db3a5c2d
woernerma <woernerma@term03.schule.lokal>
2017-05-11 10:34:36 +0200

    Initial

--- Projekt/src/projekt/Projekt.java
+++ Projekt/src/projekt/Projekt.java
@@ -15,7 +15,11 @@ public class Projekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic herefgfgdfg
+        dfg
+                df
+                g
+                        dfg
     }
     
 }
